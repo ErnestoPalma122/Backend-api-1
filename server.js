c9fddb0e535750bbeb68cf1d0ed22a2b0b15f09b
ErnestoPalma122 <112669496+ErnestoPalma122@users.noreply.github.com>
2025-07-23 17:33:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
+
